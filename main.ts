fcc39311bc605ed8d7281d50e9c93e4e20f26261
Fytecas <98255583+Fytecas@users.noreply.github.com>
2023-02-03 09:49:39 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,23 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    _4digit.show(receivedNumber)
+    if (receivedNumber > max) {
+        music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
+    } else {
+        music.stopMelody(MelodyStopOptions.All)
+    }
+})
+let max = 0
+let _4digit: grove.TM1637 = null
+music.setVolume(16)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+_4digit = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
+radio.setGroup(255)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendValue("vitmax", 255 / 1022 * pins.analogReadPin(AnalogPin.P2))
+    } else if (max != 255 / 1022 * pins.analogReadPin(AnalogPin.P2)) {
+        max = 255 / 1022 * pins.analogReadPin(AnalogPin.P2)
+        basic.showNumber(max)
+        radio.sendValue("max", max)
+    }
+})
