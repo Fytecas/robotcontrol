5715801c061d935512a8989f4e1b156880503bd8
Fytecas <98255583+Fytecas@users.noreply.github.com>
2023-03-03 19:13:05 +0100

    Restaurer '24/02/2023 09:33:59 rien du tout'

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    _4digit.show(65 / 1022 * pins.analogReadPin(AnalogPin.P2))
+    _4digit.show(receivedNumber)
     if (receivedNumber > max) {
         music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
     } else {
         music.stopMelody(MelodyStopOptions.All)
     }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onButtonPressed(Button.AB, function () {
     if (tel == 1) {
         tel = 0
     } else {
@@ -14,28 +14,18 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     }
     radio.sendValue("tel", tel)
 })
+let tel = 0
 let max = 0
 let _4digit: grove.TM1637 = null
-let tel = 0
-tel = 0
 music.setVolume(16)
 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 _4digit = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
 radio.setGroup(255)
 basic.forever(function () {
-    if (tel == 0) {
-        if (input.buttonIsPressed(Button.A)) {
-            radio.sendValue("vitmax", 40 / 1022 * pins.analogReadPin(AnalogPin.P2))
-        } else if (max != 255 / 1022 * pins.analogReadPin(AnalogPin.P2)) {
-            max = 255 / 1022 * pins.analogReadPin(AnalogPin.P2)
-            radio.sendValue("max", max)
-        }
-    } else {
-        radio.sendValue("vitmax", 40 / 1022 * pins.analogReadPin(AnalogPin.P2))
-        if (input.buttonIsPressed(Button.A)) {
-            radio.sendString("gauche")
-        } else if (input.buttonIsPressed(Button.B)) {
-            radio.sendString("droite")
-        }
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendValue("vitmax", 255 / 1022 * pins.analogReadPin(AnalogPin.P2))
+    } else if (max != 255 / 1022 * pins.analogReadPin(AnalogPin.P2)) {
+        max = 255 / 1022 * pins.analogReadPin(AnalogPin.P2)
+        radio.sendValue("max", max)
     }
 })
