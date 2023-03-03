e62a814164fc24154a18df40d55568e1894ccf23
Fytecas <98255583+Fytecas@users.noreply.github.com>
2023-03-03 09:58:21 +0100

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 def on_received_number(receivedNumber):
-    _4digit.show(receivedNumber)
+    _4digit.show(65 / 1022 * pins.analog_read_pin(AnalogPin.P2))
     if receivedNumber > max2:
         music.start_melody(music.built_in_melody(Melodies.CHASE), MelodyOptions.ONCE)
     else:
         music.stop_melody(MelodyStopOptions.ALL)
 radio.on_received_number(on_received_number)
 
-def on_button_pressed_ab():
+def on_logo_pressed():
     global tel
     if tel == 1:
         tel = 0
     else:
         tel = 1
     radio.send_value("tel", tel)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-tel = 0
 max2 = 0
 _4digit: grove.TM1637 = None
+tel = 0
+tel = 0
 music.set_volume(16)
 music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE)
 _4digit = grove.create_display(DigitalPin.P1, DigitalPin.P15)
@@ -25,9 +26,16 @@ radio.set_group(255)
 
 def on_forever():
     global max2
-    if input.button_is_pressed(Button.A):
-        radio.send_value("vitmax", 255 / 1022 * pins.analog_read_pin(AnalogPin.P2))
-    elif max2 != 255 / 1022 * pins.analog_read_pin(AnalogPin.P2):
-        max2 = 255 / 1022 * pins.analog_read_pin(AnalogPin.P2)
-        radio.send_value("max", max2)
+    if tel == 0:
+        if input.button_is_pressed(Button.A):
+            radio.send_value("vitmax", 40 / 1022 * pins.analog_read_pin(AnalogPin.P2))
+        elif max2 != 255 / 1022 * pins.analog_read_pin(AnalogPin.P2):
+            max2 = 255 / 1022 * pins.analog_read_pin(AnalogPin.P2)
+            radio.send_value("max", max2)
+    else:
+        radio.send_value("vitmax", 40 / 1022 * pins.analog_read_pin(AnalogPin.P2))
+        if input.button_is_pressed(Button.A):
+            pass
+        elif input.button_is_pressed(Button.B):
+            pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    _4digit.show(receivedNumber)
+    _4digit.show(65 / 1022 * pins.analogReadPin(AnalogPin.P2))
     if (receivedNumber > max) {
         music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
     } else {
         music.stopMelody(MelodyStopOptions.All)
     }
 })
-input.onButtonPressed(Button.AB, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (tel == 1) {
         tel = 0
     } else {
@@ -23,10 +23,19 @@ music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 _4digit = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
 radio.setGroup(255)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendValue("vitmax", 255 / 1022 * pins.analogReadPin(AnalogPin.P2))
-    } else if (max != 255 / 1022 * pins.analogReadPin(AnalogPin.P2)) {
-        max = 255 / 1022 * pins.analogReadPin(AnalogPin.P2)
-        radio.sendValue("max", max)
+    if (tel == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            radio.sendValue("vitmax", 40 / 1022 * pins.analogReadPin(AnalogPin.P2))
+        } else if (max != 255 / 1022 * pins.analogReadPin(AnalogPin.P2)) {
+            max = 255 / 1022 * pins.analogReadPin(AnalogPin.P2)
+            radio.sendValue("max", max)
+        }
+    } else {
+        radio.sendValue("vitmax", 40 / 1022 * pins.analogReadPin(AnalogPin.P2))
+        if (input.buttonIsPressed(Button.A)) {
+            radio.sendString("gauche")
+        } else if (input.buttonIsPressed(Button.B)) {
+            radio.sendString("droite")
+        }
     }
 })
